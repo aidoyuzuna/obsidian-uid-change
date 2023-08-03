2a2a9c4ad318fb00f62410e294254e9d0c6f33dc
saro <netsv1643+gi@gmail.com>
2023-08-03 16:05:33 +0900

    L15以下のインデント修正・if文の文章を簡略化

--- obsidian-uid-change.py
+++ obsidian-uid-change.py
@@ -7,17 +7,18 @@ file_list = os.listdir(dir_path)
 # ファイル名取得
 for file in file_list:
     file_name = os.path.splitext(os.path.basename(file))[0]
-    with open(dir_path + "\\" + file_name + ".md") as f:
+    with open(dir_path + "\\" + file_name + ".md", encoding="utf-8") as f:
         md_data = f.readlines()
-        # UID・日付取得
-        uid = md_data[1].replace("uid: ", "")
+    # UID・日付取得
+    uid = md_data[1].replace("uid: ", "")
     # ファイル名とUIDの検索
-    if not file_name == uid:
-        print(uid)
-        uid_white = "uid: " + file_name + "\n"
-        md_data[1] = uid_white
-        md_data[2] = "create: 2023/01/01 12:00:00\n"
-        # 書き込み
-        with open(dir_path + "\\" + file_name + ".md", mode="w") as w:
-            w.writelines(md_data)
-            print(md_data)
+    if file_name == uid:
+        continue
+    print(uid)
+    uid_white = "uid: " + file_name + "\n"
+    md_data[1] = uid_white
+    md_data[2] = "create: 2023/01/01 12:00:00\n"
+    # 書き込み
+    with open(dir_path + "\\" + file_name + ".md", mode="w") as w:
+        w.writelines(md_data)
+        print(md_data)
