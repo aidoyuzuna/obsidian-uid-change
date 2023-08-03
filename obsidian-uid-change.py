af72119dbd8354b4f206b3e147302b60ca6ce66e
saro <netsv1643+gi@gmail.com>
2023-08-03 13:20:36 +0900

    固定ディレクトリ・ファイル名で動作確認

--- obsidian-uid-change.py
+++ obsidian-uid-change.py
@@ -2,23 +2,21 @@ import os
 
 # フォルダ指定
 file_path = 'i:\\Knowledge_BackUP\\20221230_Obsidian\\01_Zettelkasten'
-file_list = os.listdir(file_path)
-md_data = ['','','']
+# file_list = os.listdir(file_path)
 
 # ファイル名取得
-for i in range(len(file_list)):
-    file_name = os.path.splitext(os.path.basename(file_list[i]))[0]
-    with open(file_path + '\\'+ file_name + '.md') as f:
-        for j in range(2):
-            md_data[j] = f.read()
-            md_data[j] = md_data[j].rstrip('\n')
+# for i in range(len(file_list)):
+file_name = os.path.splitext(os.path.basename(file_path + '\\221227144537.md'))[0]
+with open(file_path + '\\221227144537.md') as f:
+    md_data = f.readlines()
 # UID取得
-        uid = md_data[1].replace('uid: ', '')
+    uid = md_data[1].replace('uid: ', '')
 # ファイル名とUIDの検索
     if not file_name == uid:
         print(uid)
-        uid_white = 'uid: ' + file_name
+        uid_white = 'uid: ' + file_name + '\n'
         md_data[1] = uid_white
 # 書き込み
         with open(file_path + '\\'+ file_name + '.md', mode="w") as w:
             w.writelines(md_data)
+            print(md_data)
